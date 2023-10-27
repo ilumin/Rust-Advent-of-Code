88defddc845cee1f11bc8c98c5ad242feb82c05b
O Teerasak Vichadee <iolumin@gmail.com>
2023-10-27 21:17:43 +0700

    solution for 2015-day-01 part 2 is 1783

--- src/solutions/year_2015/day_01.rs
+++ src/solutions/year_2015/day_01.rs
@@ -1,5 +1,5 @@
-const OPEN_PARENTHESIS: &char = &'(';
-const CLOSE_PARENTHESIS: &char = &')';
+const OPEN_PARENTHESIS: char = '(';
+const CLOSE_PARENTHESIS: char = ')';
 
 #[allow(dead_code)]
 pub fn part_one(input: &str) -> isize {
@@ -7,10 +7,13 @@ pub fn part_one(input: &str) -> isize {
         true => 0,
         false => {
             let iter_input = input.chars();
-            let open = iter_input.clone().filter(|c| c == OPEN_PARENTHESIS).count() as isize;
+            let open = iter_input
+                .clone()
+                .filter(|c| c.to_owned() == OPEN_PARENTHESIS)
+                .count() as isize;
             let close = iter_input
                 .clone()
-                .filter(|c| c == CLOSE_PARENTHESIS)
+                .filter(|c| c.to_owned() == CLOSE_PARENTHESIS)
                 .count() as isize;
 
             open - close
@@ -18,13 +21,30 @@ pub fn part_one(input: &str) -> isize {
     }
 }
 
+fn travel(x: &char) -> isize {
+    match x.to_owned() {
+        OPEN_PARENTHESIS => 1,
+        CLOSE_PARENTHESIS => -1,
+        _ => 0,
+    }
+}
+
 #[allow(dead_code)]
-pub fn part_two() -> String {
-    todo!()
+pub fn part_two(input: &str) -> usize {
+    let mut floor = 0;
+    let position = input
+        .chars()
+        .position(|c| {
+            floor += travel(&c);
+            floor == -1
+        })
+        .unwrap();
+
+    position + 1
 }
 
 #[cfg(test)]
-mod tests {
+mod test_part_one {
     use super::*;
     use crate::problem::load_raw;
 
@@ -57,10 +77,34 @@ mod tests {
     }
 
     #[test]
-    fn day_01_part_one() {
+    fn answer() {
         let input = load_raw(2015, 1);
         let answer = part_one(&input);
-        println!("2015-day-01-part-1: {}", answer);
+        println!("ANSWER: 2015/01-1 = {}", answer);
         assert_eq!(answer, 232)
+    }
+}
+
+#[cfg(test)]
+mod test_part_two {
+    use super::*;
+    use crate::problem::load_raw;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(1, part_two(")"))
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(5, part_two("()())"))
+    }
+
+    #[test]
+    fn answer() {
+        let input = load_raw(2015, 1);
+        let answer = part_two(&input);
+        println!("ANSWER: 2015/01-2 = {}", answer);
+        assert_eq!(answer, 1783)
     }
 }

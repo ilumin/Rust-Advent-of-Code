503388655d0c224bb56b5f32d7548995ae6ac2e8
O Teerasak Vichadee <iolumin@gmail.com>
2023-10-30 15:46:37 +0700

    move load input function

--- src/solutions/year_2015/day_02.rs
+++ src/solutions/year_2015/day_02.rs
@@ -1,3 +1,21 @@
+#[allow(dead_code)]
+fn load_input(raw: &str) -> Vec<[usize; 3]> {
+    let input: Vec<[usize; 3]> = raw
+        .lines()
+        .map(|line| {
+            let mut split = line.split('x').map(|s| s.parse().unwrap());
+
+            [
+                split.next().unwrap(),
+                split.next().unwrap(),
+                split.next().unwrap(),
+            ]
+        })
+        .collect();
+
+    input
+}
+
 // find total square feet
 #[allow(dead_code)]
 pub fn part_one(input: Vec<[usize; 3]>) -> usize {
@@ -21,24 +39,6 @@ mod test_part_one {
     use crate::problem::load_raw;
 
     use super::*;
-
-    #[allow(dead_code)]
-    fn load_input(raw: &str) -> Vec<[usize; 3]> {
-        let input: Vec<[usize; 3]> = raw
-            .lines()
-            .map(|line| {
-                let mut split = line.split('x').map(|s| s.parse().unwrap());
-
-                [
-                    split.next().unwrap(),
-                    split.next().unwrap(),
-                    split.next().unwrap(),
-                ]
-            })
-            .collect();
-
-        input
-    }
 
     #[test]
     fn case_1() {
